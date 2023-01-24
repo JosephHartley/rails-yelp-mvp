6ef6fb69af45880cd7f37ebcf21830dcaf6777f3
Joe Hartley <joejohnhartley@gmail.com>
2023-01-24 20:48:09 +0000

    MVC restaurant and review

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: %i[ show edit update destroy ]
+  before_action :set_restaurant, only: %i[ show ]
 
   # GET /restaurants
   def index
@@ -15,10 +15,6 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
 
-  # GET /restaurants/1/edit
-  def edit
-  end
-
   # POST /restaurants
   def create
     @restaurant = Restaurant.new(restaurant_params)
@@ -30,29 +26,14 @@ class RestaurantsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /restaurants/1
-  def update
-    if @restaurant.update(restaurant_params)
-      redirect_to @restaurant, notice: "Restaurant was successfully updated."
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /restaurants/1
-  def destroy
-    @restaurant.destroy
-    redirect_to restaurants_url, notice: "Restaurant was successfully destroyed."
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_restaurant
-      @restaurant = Restaurant.find(params[:id])
-    end
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 
     # Only allow a list of trusted parameters through.
-    def restaurant_params
-      params.require(:restaurant).permit(:name, :address, :phone_number, :category)
-    end
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "The Happy Sumo", address: "7 Boundary St, London E2 7JE", category: "japanese"}
+pizza_east = {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian"}
+h2u = {name: "H-2-U", address: "25 Staplehurst Rd, London SE13 5ND", category: "chinese"}
+casse = {name: "Casse-Croûte", address: "109 Bermondsey St, London SE1 3XB", category: "french"}
+lowlander = {name: "Lowlander Grand Cafe
+  ", address: "36 Drury Ln, London WC2B 5RR", category: "belgian"}
+
+[dishoom, pizza_east, h2u, casse, lowlander].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
